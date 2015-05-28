5d12740cd966cc3575da9582083898dc8657e4f4
Chi Yang <chi.chloe.yang@gmail.com>
2015-05-28 09:36:47 -0700

    rename the custom annotation,(missing a t :p)

---
+++ GetOnThatBus/BusStopMKPointAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  BusStopMKPointAnnotaion.m
+//  GetOnThatBus
+//
+//  Created by Chi Yang on 5/27/15.
+//  Copyright (c) 2015 Chee Vue. All rights reserved.
+//
+
+#import "BusStopMKPointAnnotation.h"
+
+@implementation BusStopMKPointAnnotation
+
+@end
